681719e83c7a17ab9225426327db10b372769a80
Filippo-Timo <filtimo2002@gmail.com>
2026-01-13 14:35:42 +0100

    Finishes first exercise

---
+++ src/Esercizio1/Es1.java
@@ -0,0 +1,34 @@
+package Esercizio1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Es1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        HashSet<String> set = new HashSet<>();
+        HashSet<String> paroleRipetute = new HashSet<>();
+
+        int numElementi = 0;
+
+        System.out.println("Inserisci un numero che corrisponderà al numero di elementi del da inserire: ");
+
+        try {
+            numElementi = Integer.parseInt(input.nextLine());
+        } catch (Exception e) {
+            System.out.println("Il valore inserito non è valido");
+        }
+
+        for (int i = 0; i <= numElementi; i++) {
+            System.out.println("Ora inserisci una parola da inserire nel set: ");
+            String parola = input.nextLine();
+            if (set.contains(parola)) paroleRipetute.add(parola);
+            else set.add(parola);
+        }
+
+        System.out.println("Le/a parole/a ripetute/a sono: " + paroleRipetute.toString());
+        System.out.println("Il numero di parole distinte è " + set.size());
+        System.out.println("Il set che hai formato è: " + set.toString());
+    }
+}
